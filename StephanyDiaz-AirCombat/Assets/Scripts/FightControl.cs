5114ea98e54c3b7ada0f6181f6b6a53474dda982
Stephany Diaz Cartagena <asocoba96@hotmail.com>
2018-10-06 13:49:03 -0500

    1

---
+++ StephanyDiaz-AirCombat/Assets/Scripts/FightControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightControl : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Translate(Vector3.forward * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detectada");
+
+        GameObject enemy = collision.collider.gameObject;
+
+
+        GameObject missile = this.gameObject;
+
+        GameObject.Destroy(enemy);
+        GameObject.Destroy(missile);
+        {
+        }
+    }
+}
+
---
+++ StephanyDiaz-AirCombat/Assets/Scripts/Missil.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missil : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Translate(Vector3.forward * speed);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shoot");
+
+            GameObject newMissile = GameObject.Instantiate(Resources.Load("Bullets/missile") as GameObject);
+
+            GameObject ShootPivote = GameObject.Find("fighter/ShootPivote");
+
+            newMissile.transform.SetParent(ShootPivote.transform);
+            newMissile.transform.localPosition = Vector3.zero;
+            newMissile.transform.SetParent(null);
+        }
+    }
+
+
+
+}
+
